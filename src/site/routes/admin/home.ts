0adbbdee5b0272f3fbc461f81c3cd95e635bac84
Peter Hulme <github@peterhul.me>
2021-10-15 16:20:29 +0100

    Removed admin home page and added links to main nav

--- src/site/routes/admin/home.ts
+++
@@ -1,3 +0,0 @@
-export default (req: any, res: any) => {
-  res.render("admin/home", { section: "admin", isAdmin: req.session.isAdmin });
-};
--- src/site/routes/admin/index.ts
+++ src/site/routes/admin/index.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import home from "./home";
 import questions from "./questions";
 import users from "./users";
 import standings from "./standings";
 
 const routes = Router();
-
-routes.get("/", home);
 
 routes.use("/questions", questions);
 routes.use("/users", users);
